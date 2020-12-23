07295d8b378fa14bfcf723a14d0d3deaa5284275
Ernest <33957800+ErnestMironov@users.noreply.github.com>
2020-12-23 19:13:58 +0300

    Updates

---
+++ precache-manifest.c9c5e5853e753624760bb9419988b1f2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc32813655aa8df3c82",
+    "url": "/contacts-book/css/app.743141b0.css"
+  },
+  {
+    "revision": "c70cdedf4d6c5af208c2",
+    "url": "/contacts-book/css/chunk-vendors.71a233fb.css"
+  },
+  {
+    "revision": "c30ba9a20629e02ff0e004a1521032cf",
+    "url": "/contacts-book/img/arrow.c30ba9a2.svg"
+  },
+  {
+    "revision": "16f0fde2290a4b19fddb8c3571d0ce64",
+    "url": "/contacts-book/img/logo.16f0fde2.svg"
+  },
+  {
+    "revision": "67a53557c2833ec9c3fafb79a9bb842e",
+    "url": "/contacts-book/img/pencil.67a53557.svg"
+  },
+  {
+    "revision": "d92c7b6799ea7135b91db1c8f9696f5d",
+    "url": "/contacts-book/img/plus.d92c7b67.svg"
+  },
+  {
+    "revision": "f8c56c322528f0bc108bef92dc2a7f19",
+    "url": "/contacts-book/img/times.f8c56c32.svg"
+  },
+  {
+    "revision": "39190e048ad8ff0a478b7c7ca8022164",
+    "url": "/contacts-book/index.html"
+  },
+  {
+    "revision": "7fc32813655aa8df3c82",
+    "url": "/contacts-book/js/app.dd726a77.js"
+  },
+  {
+    "revision": "c70cdedf4d6c5af208c2",
+    "url": "/contacts-book/js/chunk-vendors.c5d4461b.js"
+  },
+  {
+    "revision": "d198a610d558073ae0ddfdbd49e7999c",
+    "url": "/contacts-book/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/contacts-book/robots.txt"
+  }
+]);
